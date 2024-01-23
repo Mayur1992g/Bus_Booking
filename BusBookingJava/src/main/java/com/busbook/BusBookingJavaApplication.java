7319b56c5bad22cf887c994c47819dece1c79efe
Mayur900 <128124045+Mayur900@users.noreply.github.com>
2024-01-23 17:54:22 +0530

    springboot comit

---
+++ BusBookingJava/src/main/java/com/busbook/BusBookingJavaApplication.java
@@ -0,0 +1,13 @@
+package com.busbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusBookingJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusBookingJavaApplication.class, args);
+	}
+
+}
---
+++ BusBookingJava/src/test/java/com/busbook/BusBookingJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusBookingJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
